cb77d56cdd6d9cea54a41d96874cd6f2b4cd9cd5
JamesTaylor <hoijf666@gmail.com>
2017-07-19 10:25:32 +0100

    Basic structure prepared

--- src/FileOrganizer/FileOrganize.java
+++ src/FileOrganizer/FileOrganize.java
@@ -4,6 +4,7 @@ public class FileOrganize
 {
 	public static void main(String[] args)
 	{
-		
+		Manager manager = new Manager();
+		manager.begin();
 	}
 }
---
+++ src/FileOrganizer/Manager.java
@@ -0,0 +1,19 @@
+package FileOrganizer;
+
+import IO.PrepareIO;
+
+public class Manager
+{
+	//Instances
+	private PrepareIO prepIO;
+	
+	public Manager()
+	{
+		
+	}
+	
+	public void begin()
+	{
+		
+	}
+}
